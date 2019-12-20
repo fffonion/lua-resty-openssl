714a1e541e0ce3ffb33d257d9af50ae628094fb2
Wangchong Zhou <fffonion@gmail.com>
2019-12-20 15:24:18 -0800

    feat(x509.name) allow to iterate over objects and find objects

--- lib/resty/openssl/x509/name.lua
+++ lib/resty/openssl/x509/name.lua
@@ -1,16 +1,62 @@
 local ffi = require "ffi"
 local C = ffi.C
 local ffi_gc = ffi.gc
+local ffi_str = ffi.string
+local ffi_sizeof = ffi.sizeof
 
 require "resty.openssl.include.x509.name"
+local asn1_macro = require "resty.openssl.include.asn1"
 
 -- local MBSTRING_FLAG = 0x1000
 local MBSTRING_ASC  = 0x1001 -- (MBSTRING_FLAG|1)
 
 local _M = {}
-local mt = { __index = _M, __tostring = tostring }
 
 local x509_name_ptr_ct = ffi.typeof("X509_NAME*")
+
+local char_ptr = ffi.typeof('char[?]')
+-- starts from 0
+local function value_at(ctx, i)
+  local entry = C.X509_NAME_get_entry(ctx, i)
+  local obj = C.X509_NAME_ENTRY_get_object(entry)
+  local nid = C.OBJ_obj2nid(obj)
+
+  local buf = char_ptr(100)
+  local len = C.OBJ_obj2txt(buf, ffi_sizeof(buf), obj, 1)
+  local oid = ffi_str(buf, len)
+
+  local str = C.X509_NAME_ENTRY_get_data(entry)
+  local blob
+  if str ~= nil then
+    blob = ffi_str(asn1_macro.ASN1_STRING_get0_data(str))
+  end
+
+  return {
+    id = oid,
+    nid = nid,
+    sn = ffi_str(C.OBJ_nid2sn(nid)),
+    ln = ffi_str(C.OBJ_nid2ln(nid)),
+    blob = blob,
+  }
+end
+
+local function iter(tbl)
+  local i = 0
+  local n = tonumber(C.X509_NAME_entry_count(tbl.ctx))
+  return function()
+    i = i + 1
+    if i <= n then
+      local obj = value_at(tbl.ctx, i-1)
+      return obj.sn or obj.ln or obj.id, obj
+    end
+  end
+end
+
+local mt = {
+  __index = _M,
+  __pairs = iter,
+  __len = function(tbl) return tonumber(C.X509_NAME_entry_count(tbl.ctx)) end,
+}
 
 function _M.new()
   local ctx = C.X509_NAME_new()
@@ -30,6 +76,20 @@ function _M.istype(l)
   return l and l.ctx and ffi.istype(x509_name_ptr_ct, l.ctx)
 end
 
+function _M.dup(ctx)
+  if not ffi.istype(x509_name_ptr_ct, ctx) then
+    return nil, "expect a x509.name ctx at #1"
+  end
+  local ctx = C.X509_NAME_dup(ctx)
+  ffi_gc(ctx, C.X509_NAME_free)
+
+  local self = setmetatable({
+    ctx = ctx,
+  }, mt)
+
+  return self, nil
+end
+
 function _M:add(nid, txt)
   local asn1 = C.OBJ_txt2obj(nid, 0)
   if asn1 == nil then
@@ -46,19 +106,39 @@ function _M:add(nid, txt)
   return self
 end
 
-function _M.dup(ctx)
-  if not ffi.istype(x509_name_ptr_ct, ctx) then
-    return nil, "expect a x509.name ctx at #1"
+function _M:find(nid, last_post)
+  local asn1 = C.OBJ_txt2obj(nid, 0)
+  if asn1 == nil then
+    return nil, nil, "invalid NID text " .. (nid or "nil")
   end
-  local ctx = C.X509_NAME_dup(ctx)
-  ffi_gc(ctx, C.X509_NAME_free)
+  -- make 1-index array to 0-index
+  last_post = (last_post or 0) - 1
 
-  local self = setmetatable({
-    ctx = ctx,
-  }, mt)
+  local idx = C.X509_NAME_get_index_by_OBJ(self.ctx, asn1, last_post)
+  if idx == -1 then
+    return nil, nil, nil
+  end
 
-  return self, nil
+  return value_at(self.ctx, idx), idx + 1
 end
 
+-- fallback function to iterate if LUAJIT_ENABLE_LUA52COMPAT not enabled
+function _M:all()
+  local ret = {}
+  local next_ = iter(self)
+  while true do
+    local k, obj = next_()
+    if obj then
+      ret[k] = obj
+    else
+      break
+    end
+  end
+  return ret
+end
+
+function _M:each()
+  return iter(self)
+end
 
 return _M
--- t/openssl/x509/name.t
+++ t/openssl/x509/name.t
@@ -69,3 +69,94 @@ invalid NID text whatever
 "
 --- no_error_log
 [error]
+
+=== TEST 3: Finds by text
+--- http_config eval: $::HttpConfig
+--- config
+    location =/t {
+        content_by_lua_block {
+            local name, err = require("resty.openssl.x509.name").new()
+            if err then
+                ngx.log(ngx.ERR, err)
+                return
+            end
+            name, err = name:add("CN", "example.com")
+            if err then
+                ngx.log(ngx.ERR, err)
+                return
+            end
+            name, err = name:add("CN", "anotherdomain.com")
+            if err then
+                ngx.log(ngx.ERR, err)
+                return
+            end
+
+            local a, b, c = name:find("CN")
+            if a then
+                ngx.say("found ", b, " ", a.blob)
+            end
+            local a, b, c = name:find("2.5.4.3")
+            if a then
+                ngx.say("found ", b, " ", a.blob)
+            end
+            local a, b, c = name:find("CM")
+            if not a then
+                ngx.say("not found")
+            end
+            local a, b, c = name:find("CN", 1)
+            if a then
+                ngx.say("found ", b, " ", a.blob)
+            end
+        }
+    }
+--- request
+    GET /t
+--- response_body_like eval
+"found 1 example.com
+found 1 example.com
+not found
+found 2 anotherdomain.com
+"
+--- no_error_log
+[error]
+
+
+=== TEST 4: Pairs
+--- http_config eval: $::HttpConfig
+--- config
+    location =/t {
+        content_by_lua_block {
+            local name, err = require("resty.openssl.x509.name").new()
+            if err then
+                ngx.log(ngx.ERR, err)
+                return
+            end
+            local CNs = 3
+            for i=1,CNs,1 do
+                name, err = name:add("CN", string.format("%d.example.com", i))
+                if err then
+                    ngx.log(ngx.ERR, err)
+                    return
+                end
+            end
+            local others = { "L", "ST", "O" }
+            for _, k in ipairs(others) do
+                name, err = name:add(k, "Mars")
+                if err then
+                    ngx.log(ngx.ERR, err)
+                    return
+                end
+            end
+            ngx.say(#name)
+            for k, v in pairs(name) do
+                ngx.print(v.nid .. ",")
+            end
+        }
+    }
+--- request
+    GET /t
+--- response_body eval
+"6
+13,13,13,15,16,17,"
+--- no_error_log
+[error]
