57815dd38bbb2e260e8cdf3e8ddac48d6254b8fc
Wangchong Zhou <fffonion@gmail.com>
2020-01-06 16:47:17 -0800

    fix(x509.store) memory leak in store:add

--- lib/resty/openssl/x509/store.lua
+++ lib/resty/openssl/x509/store.lua
@@ -22,6 +22,8 @@ function _M.new()
 
   local self = setmetatable({
     ctx = ctx,
+    _x509_refs = {},
+    _x509_refs_idx = 1,
   }, mt)
 
   return self, nil
@@ -48,6 +50,10 @@ function _M:add(item)
       C.X509_free(dup)
       return false, format_error("store:add: X509_STORE_add_cert")
     end
+    -- gc added X509 objects together with X509_store
+    self._x509_refs[self._x509_refs_idx] = dup
+    self._x509_refs_idx = self._x509_refs_idx + 1
+    ffi_gc(dup, C.X509_free)
   else
     return false, "expect a x509 instance at #1"
   end
--- t/openssl/x509/name.t
+++ t/openssl/x509/name.t
@@ -30,6 +30,7 @@ __DATA__
                 ngx.log(ngx.ERR, err)
                 return
             end
+            -- valgrind might warn about double free on this test because of the following
             require('ffi').C.X509_NAME_free(name.ctx)
             -- if name2.ctx is also freed this following will segfault
             local _, err = name2:add("CN", "example.com")
