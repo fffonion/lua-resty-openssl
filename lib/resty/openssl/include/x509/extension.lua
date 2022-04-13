0946c5937fa9fa4bb41a70267a67fcc87307b6a6
Wangchong Zhou <fffonion@gmail.com>
2022-04-13 06:54:41 -0700

    fix(x509.extension) correct X509V3_CTX size for OpenSSL 3.0

--- lib/resty/openssl/include/x509/extension.lua
+++ lib/resty/openssl/include/x509/extension.lua
@@ -4,11 +4,13 @@ require "resty.openssl.include.ossl_typ"
 require "resty.openssl.include.x509v3"
 require "resty.openssl.include.x509"
 local asn1_macro = require "resty.openssl.include.asn1"
+local OPENSSL_30 = require("resty.openssl.version").OPENSSL_30
 
 asn1_macro.declare_asn1_functions("X509_EXTENSION")
 
-ffi.cdef [[
-  struct v3_ext_ctx {
+if OPENSSL_30 then
+  ffi.cdef [[
+    struct v3_ext_ctx {
       int flags;
       X509 *issuer_cert;
       X509 *subject_cert;
@@ -16,7 +18,27 @@ ffi.cdef [[
       X509_CRL *crl;
       /*X509V3_CONF_METHOD*/ void *db_meth;
       void *db;
-  };
+      EVP_PKEY *issuer_pkey;
+    };
+
+    int X509V3_set_issuer_pkey(X509V3_CTX *ctx, EVP_PKEY *pkey);
+  ]]
+
+else
+  ffi.cdef [[
+    struct v3_ext_ctx {
+      int flags;
+      X509 *issuer_cert;
+      X509 *subject_cert;
+      X509_REQ *subject_req;
+      X509_CRL *crl;
+      /*X509V3_CONF_METHOD*/ void *db_meth;
+      void *db;
+    };
+  ]]
+end
+
+ffi.cdef [[
   int X509_EXTENSION_set_data(X509_EXTENSION *ex, ASN1_OCTET_STRING *data);
   int X509_EXTENSION_set_object(X509_EXTENSION *ex, const ASN1_OBJECT *obj);
 ]]
