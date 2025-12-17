83399f5f51954ad93d82ed0e0746dea366fb586f
Wangchong Zhou <fffonion@gmail.com>
2025-12-17 14:13:41 +0800

    Update lib/resty/openssl/kdf.lua

--- lib/resty/openssl/kdf.lua
+++ lib/resty/openssl/kdf.lua
@@ -140,6 +140,7 @@ function _M.derive(options)
   end
 
   for k, v in pairs(options_schema) do
+    # don't use unpack here to avoid nil truncation
     local v, err = check_options(options, typ, k, v[1],v[2],v[3])
     if err then
       return nil, "kdf.derive: " .. err
